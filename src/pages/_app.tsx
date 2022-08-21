61110515cb907d20874cf0398533c4842ca5c635
SrJuggernaut <jugger@srjuggernaut.dev>
2022-08-21 15:08:22 -0500

    feat: fontawesome, gsap, bundle analyzer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import { config } from '@fortawesome/fontawesome-svg-core'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
 
 import theme from '@styles/muiTheme'
+
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+config.autoAddCss = false
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
