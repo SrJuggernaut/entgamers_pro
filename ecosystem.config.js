09cf3d02f3b68b5aacf909baffb8403d390bb426
SrJuggernaut <jugger@srjuggernaut.dev>
2022-09-29 18:06:01 -0500

    ci: deployment to pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  apps: [
+    {
+      name: process.env.APP_NAME,
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: process.env.PORT || 3000
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: process.env.SSH_USERNAME,
+      host: process.env.SSH_HOST,
+      ref: 'origin/main',
+      repo: 'https://github.com/SrJuggernaut/entgamers_pro',
+      path: process.env.DEPLOY_PATH,
+      'pre-deploy': 'npm install && npm run build',
+      'post-deploy': 'pm2 startOrRestart ecosystem.config.js',
+      env: {
+        ...process.env
+      }
+
+    },
+    preview: {
+      user: process.env.SSH_USERNAME,
+      host: process.env.SSH_HOST,
+      ref: 'origin/preview',
+      repo: 'https://github.com/SrJuggernaut/entgamers_pro',
+      path: process.env.DEPLOY_PATH,
+      'pre-deploy': 'npm install && npm run build',
+      'post-deploy': 'pm2 startOrRestart ecosystem.config.js',
+      env: {
+        ...process.env
+      }
+    }
+  }
+}
