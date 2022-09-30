c69a166c1f59bf9cf8eb49b8c48e4a0453a807ba
Jugger <jugger@srjuggernaut.dev>
2022-09-30 18:23:38 -0500

    revert: deploy as sudo
    Refs: 624b225

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       repo: 'https://github.com/SrJuggernaut/entgamers_pro',
       path: process.env.DEPLOY_PATH,
       'pre-deploy': 'npm install && npm run build',
-      'post-deploy': 'sudo pm2 startOrRestart ecosystem.config.js',
+      'post-deploy': 'pm2 startOrRestart ecosystem.config.js',
       env: {
         APP_NAME: process.env.APP_NAME,
         PORT: process.env.PORT,
@@ -35,7 +35,7 @@ module.exports = {
       repo: 'https://github.com/SrJuggernaut/entgamers_pro',
       path: process.env.DEPLOY_PATH,
       'pre-deploy': 'npm install && npm run build',
-      'post-deploy': 'sudo pm2 startOrRestart ecosystem.config.js',
+      'post-deploy': 'pm2 startOrRestart ecosystem.config.js',
       env: {
         APP_NAME: process.env.APP_NAME,
         PORT: process.env.PORT,
