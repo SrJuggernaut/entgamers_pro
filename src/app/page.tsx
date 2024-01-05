a6e072703d70747338913712882af998f9278bc9
SrJuggernaut <jugger@srjuggernaut.dev>
2024-01-04 22:11:10 -0600

    feat: update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,9 @@ import Clanes from '@/app/Clanes'
 import Hero from '@/app/Hero'
 import Social from '@/app/Social'
 import Team from '@/app/Team'
-import { ensureRoles } from '@/services/backend/roles'
-import { ensureTeamApplyCollection } from '@/services/backend/teamApply'
 import { type FC } from 'react'
 
-const ensureAll = async (): Promise<void> => {
-  await ensureRoles()
-  await ensureTeamApplyCollection()
-}
-
 const HomePage: FC = async () => {
-  await ensureAll()
   return (
     <>
       <Hero />
