71c14fee52f673a1f8af066a94093f458d822355
SrJuggernaut <jugger@srjuggernaut.dev>
2024-01-27 14:08:36 -0600

    chore: updated dependencies

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 
+const imageDomains = (process.env.IMAGE_DOMAINS ?? '').split(',').map(domain => domain.trim())
+
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  images: {
+    domains: imageDomains
+  }
 }
